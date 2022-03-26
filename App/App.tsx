fb842ede29d153191a06ce20e8e81331c9fd99c7
Diego Gallovich <dieguiviti@gmail.com>
2022-03-26 17:53:55 +0100

    Implementing stack navigator with a starter view of home screen

--- App/App.tsx
+++ App/App.tsx
@@ -9,8 +9,33 @@
  */
 
 import * as React from 'react';
+import {View, Text, ViewStyle} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const homeScreenStyles: ViewStyle = {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const HomeScreen = (): JSX.Element => {
+  return (
+    <View style={homeScreenStyles}>
+      <Text>NFC + Navigation Sample App</Text>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <NavigationContainer>{/* Rest of app code */}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
